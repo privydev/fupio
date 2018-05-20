8ced032a0c32efd2341dcba82449fce684afaed6
Mehmet Kose <mehmetkose122@gmail.com>
2018-05-21 01:17:24 +0300

    firebase updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
-import firebase from 'firebase'
+// import firebase from 'firebase'
+
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 
 //const firebase = window.firebase
 
